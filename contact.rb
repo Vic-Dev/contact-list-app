b9d66f9455d704b9653d9a78e1830a82d58f91e7
Victoria Johns <vmichellej@gmail.com>
2016-05-10 12:24:25 -0400

    Add Gemfile and connection class method

--- contact.rb
+++ contact.rb
@@ -1,27 +1,27 @@
 require 'csv'
+require 'pg'
 
-# Represents a person in an address book.
-# The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
   attr_accessor :name, :email
   
-  # Creates a new contact object
-  # @param name [String] The contact's name
-  # @param email [String] The contact's email address
   def initialize(name, email)
-    # TODO: Assign parameter values to instance variables.
     @name = name
     @email = email
   end
 
-  # Provides functionality for managing contacts in the csv file.
   class << self
 
-    # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
-    # @return [Array<Contact>] Array of Contact objects
+    def connection
+      PG.connect(
+        host: 'localhost'
+        dbname: 'contact_list'
+        user: 'development'
+        password: 'development'
+      )
+    end
+
     def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
       array = []
       CSV.foreach('customers.csv') do |name, email|
         array << Contact.new(name, email)
@@ -29,13 +29,7 @@ class Contact
       array
     end
 
-    # Creates a new contact, adding it to the csv file, returning the new contact.
-    # @param name [String] the new contact's name
-    # @param email [String] the contact's email
     def create(name, email)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
-      # If a user tries to input the the same email address for a new contact, output an error saying that the contact already exists and cannot be created.
-
       if email_exists?(email)
         "Error: contact already exists"
       else
@@ -51,28 +45,18 @@ class Contact
       CSV.read('customers.csv').any? {|x| x.include? email}
     end
     
-    # Find the Contact in the 'contacts.csv' file with the matching id.
-    # @param id [Integer] the contact id
-    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
       id_index = id - 1
       Contact.all[id_index]
     end
     
-    # Search for contacts by either name or email.
-    # @param term [String] the name fragment or email fragment to search for
-    # @return [Array<Contact>] Array of Contact objects.
     def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
       array = []
       search = CSV.read('customers.csv')
       thing1 = search.each do |contact| 
         contact_string = contact.join(" ")
         if contact_string.downcase.include? term.downcase
           array << contact
-          # string = "#{index + 1}: #{contact[0]} #{contact[1]}"
-          # array << string
         end
       end      
       array
